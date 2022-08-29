756ee33e052abf0a92b8dd4d78e3f0dd9340168c
Matthew Miller <mwm@fastmail.com>
2022-08-29 13:07:56 +0200

    Clean up events with values
    I figured this had to be possible, so I just tried it. Yay!

--- lib/catenary_web/live.ex
+++ lib/catenary_web/live.ex
@@ -3,6 +3,8 @@ defmodule CatenaryWeb.Live do
 
   # Every 9s or so, we see if someone put new stuff in the store
   @store_refresh 9001
+  # Mostly making sure these exist, but also faux docs
+  @sort_atoms [:asc, :dec, :author, :logid, :seq]
 
   def mount(_params, _session, socket) do
     starting_sort = [dir: :dec, by: :seq]
@@ -29,9 +31,9 @@ defmodule CatenaryWeb.Live do
     <div class="mx-2 grid grid-cols-1 md:grid-cols-4 gap-10 justify-center font-mono">
        <table class="my-4">
          <tr>
-           <th><button phx-click="asc_author">↓</button> Author <button phx-click="dec_author">↑</button></th>
-           <th><button phx-click="asc_logid">↓</button> Log Id <button phx-click="dec_logid">↑</button></th>
-           <th><button phx-click="asc_seq">↓</button> Max Seq <button phx-click="dec_seq">↑</button></th>
+           <th><button value="asc-author" phx-click="sort">↓</button> Author <button value="dec-author" phx-click="sort">↑</button></th>
+           <th><button value="asc-logid" phx-click="sort">↓</button> Log Id <button value="dec-logid" phx-click="sort">↑</button></th>
+           <th><button value="asc-seq" phx-click="sort">↓</button> Max Seq <button value="dec-seq" phx-click="sort">↑</button></th>
          </tr>
          <%= for {author, log_id, seq} <- @store do %>
          <tr><td><%= String.slice(author, 0..15) %></td><td><%= log_id %></td><td><%= seq %></td></tr>
@@ -41,33 +43,8 @@ defmodule CatenaryWeb.Live do
     """
   end
 
-  def handle_event("asc_author", _value, socket) do
-    sorter = [dir: :asc, by: :author]
-    {:noreply, assign(socket, store: sorted_store(socket.assigns.store, sorter), sorter: sorter)}
-  end
-
-  def handle_event("dec_author", _value, socket) do
-    sorter = [dir: :dec, by: :author]
-    {:noreply, assign(socket, store: sorted_store(socket.assigns.store, sorter), sorter: sorter)}
-  end
-
-  def handle_event("asc_logid", _value, socket) do
-    sorter = [dir: :asc, by: :logid]
-    {:noreply, assign(socket, store: sorted_store(socket.assigns.store, sorter), sorter: sorter)}
-  end
-
-  def handle_event("dec_logid", _value, socket) do
-    sorter = [dir: :dec, by: :logid]
-    {:noreply, assign(socket, store: sorted_store(socket.assigns.store, sorter), sorter: sorter)}
-  end
-
-  def handle_event("asc_seq", _value, socket) do
-    sorter = [dir: :asc, by: :seq]
-    {:noreply, assign(socket, store: sorted_store(socket.assigns.store, sorter), sorter: sorter)}
-  end
-
-  def handle_event("dec_seq", _value, socket) do
-    sorter = [dir: :dec, by: :seq]
+  def handle_event("sort", %{"value" => <<dir::binary-size(3), "-", by::binary>>}, socket) do
+    sorter = [dir: String.to_existing_atom(dir), by: String.to_existing_atom(by)]
     {:noreply, assign(socket, store: sorted_store(socket.assigns.store, sorter), sorter: sorter)}
   end
 
