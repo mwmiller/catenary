3c1de1687ca134997e7c1e62e24c3972749f3847
Matthew Miller <mwm@fastmail.com>
2022-08-30 17:53:01 +0200

    Tweak the UI a bit
    The reported issue is "recent oasis" duplication. Taking this
    opportunity to fix a bunch of other niggling issues
    
    All horribly factored, but that's not the point.

--- lib/catenary_web/live.ex
+++ lib/catenary_web/live.ex
@@ -5,9 +5,9 @@ defmodule CatenaryWeb.Live do
   @store_refresh 9001
 
   def mount(_params, _session, socket) do
-    starting_sort = [dir: :dec, by: :seq]
+    starting_sort = [dir: :desc, by: :seq]
     # Making sure these exist, but also faux docs
-    [:asc, :dec, :author, :logid, :seq]
+    [:asc, :desc, :author, :logid, :seq]
 
     {store, watering} =
       case connected?(socket) do
@@ -32,9 +32,9 @@ defmodule CatenaryWeb.Live do
       <div>
         <%= for {recent, index}  <- Enum.with_index(@watering) do %>
           <div class="<%= case rem(index, 2)  do
-         0 ->  "bg-emerald-200 dark:bg-cyan-700"
+          0 ->  "bg-emerald-200 dark:bg-cyan-700"
           1 -> "bg-emerald-400 dark:bg-sky-700"
-        end %>"><%= recent["name"] %><br><%= recent["host"]<>":"<>Integer.to_string(recent["port"]) %></div>
+        end %>"><span title="as of: <%= ago_string(recent.age, 2)%>"><%= recent["name"] %> (<%= recent.id %>)</span><br><%= recent["host"]<>":"<>Integer.to_string(recent["port"]) %></div>
         <% end %>
       </div>
       <div>
@@ -45,7 +45,7 @@ defmodule CatenaryWeb.Live do
            <th><button value="asc-seq" phx-click="sort">↓</button> Max Seq <button value="dec-seq" phx-click="sort">↑</button></th>
          </tr>
          <%= for {author, log_id, seq} <- @store do %>
-         <tr><td><%= String.slice(author, 0..15) %></td><td><%= log_id %></td><td><%= seq %></td></tr>
+         <tr align="center"><td><%= short_id(author)  %></td><td><%= log_id %></td><td><%= seq %></td></tr>
          <% end %>
       </table>
     </div>
@@ -70,7 +70,15 @@ defmodule CatenaryWeb.Live do
     |> extract_recents(DateTime.now!("Etc/UTC"), [])
   end
 
-  defp extract_recents([], _, acc), do: acc
+  defp extract_recents([], _, acc) do
+    # Put them in age order
+    # Pick the most recent for any host/port dupes
+    # Display a max of 3
+    acc
+    |> Enum.sort_by(fn m -> Map.get(m, :age) end, :asc)
+    |> Enum.uniq_by(fn %{"host" => h, "port" => p} -> {h, p} end)
+    |> Enum.take(4)
+  end
 
   defp extract_recents([{a, l, e} | rest], now, acc) do
     try do
@@ -80,11 +88,14 @@ defmodule CatenaryWeb.Live do
       case map do
         %{"running" => ts} ->
           {:ok, then, _offset} = DateTime.from_iso8601(ts)
-          sago = DateTime.diff(then, now)
+          sago = DateTime.diff(now, then)
 
           cond do
-            sago > -172_800 -> extract_recents(rest, now, [map | acc])
-            true -> extract_recents(rest, now, acc)
+            sago > -172_800 ->
+              extract_recents(rest, now, [Map.merge(map, %{age: sago, id: short_id(a)}) | acc])
+
+            true ->
+              extract_recents(rest, now, acc)
           end
 
         _ ->
@@ -94,6 +105,8 @@ defmodule CatenaryWeb.Live do
       _ -> extract_recents(rest, now, acc)
     end
   end
+
+  defp short_id(id), do: "~" <> String.slice(id, 0..15)
 
   defp sorted_store(store, opts) do
     elem =
@@ -106,7 +119,7 @@ defmodule CatenaryWeb.Live do
     comp =
       case Keyword.get(opts, :dir) do
         :asc -> &Kernel.<=/2
-        :dec -> &Kernel.>=/2
+        :desc -> &Kernel.>=/2
       end
 
     # The extra step keeps it stable across
@@ -119,5 +132,31 @@ defmodule CatenaryWeb.Live do
     |> Enum.reject(fn {_, l, _} -> l == 8483 end)
     |> Enum.sort_by(fn {a, _, _} -> a end, &Kernel.<=/2)
     |> Enum.sort_by(elem, comp)
+  end
+
+  defp ago_string(sago, n) do
+    "about " <> compile_sections(sago, n, []) <> " ago"
+  end
+
+  defp compile_sections(sago, _, acc) when sago <= 0, do: acc |> Enum.reverse() |> Enum.join("")
+  defp compile_sections(_sago, n, acc) when length(acc) == n, do: compile_sections(0, n, acc)
+
+  defp compile_sections(sago, n, acc) when div(sago, 86400) > 0 do
+    u = div(sago, 86400)
+    compile_sections(sago - 86400 * u, n, [Integer.to_string(u) <> "d" | acc])
+  end
+
+  defp compile_sections(sago, n, acc) when div(sago, 3600) > 0 do
+    u = div(sago, 3600)
+    compile_sections(sago - 3600 * u, n, [Integer.to_string(u) <> "h" | acc])
+  end
+
+  defp compile_sections(sago, n, acc) when div(sago, 60) > 0 do
+    u = div(sago, 60)
+    compile_sections(sago - 60 * u, n, [Integer.to_string(u) <> "m" | acc])
+  end
+
+  defp compile_sections(sago, n, acc) do
+    compile_sections(0, n, [Integer.to_string(sago) <> "s" | acc])
   end
 end
