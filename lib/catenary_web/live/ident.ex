7ec302a75e73960fbd7f61d44c3b3a01fd4ffdc4
Matthew Miller <mwm@fastmail.com>
2022-10-06 14:05:10 +0200

    Update deps
    And fix a minor compliation warning

--- lib/catenary_web/live/ident.ex
+++ lib/catenary_web/live/ident.ex
@@ -21,6 +21,7 @@ defmodule Catenary.Live.Ident do
   end
 
   # This seems a bit convoluted, but I want to reuse the menu logic
+  @impl true
   def handle_event("to-im", _, socket) do
     Phoenix.PubSub.local_broadcast(Catenary.PubSub, "ui", %{view: :idents})
     {:noreply, socket}
