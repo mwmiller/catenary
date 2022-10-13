9f9a1eaaa4bf3a7e2517a6df63134d005669ec7f
Matthew Miller <mwm@fastmail.com>
2022-10-13 17:32:59 +0200

    Change the shown button senses
    They are terrifyingly jumpt today.  Design help needed.

--- lib/catenary_web/live/prefsmanager.ex
+++ lib/catenary_web/live/prefsmanager.ex
@@ -37,8 +37,8 @@ defmodule Catenary.Live.PrefsManager do
     </div>
     </div>
     <div class="flex flex-row min-w-full">
-      <div class="flex-auto"><button class="border opacity-61 p-2 m-10 bg-slate-200 dark:bg-slate-800" value="all" phx-click="shown">Mark all logs shown</button></div>
-      <div class="flex-auto"><button class="border opacity-61 p-2 m-10 bg-slate-200 dark:bg-slate-800" value="none" phx-click="shown">Mark all logs unshown</button></div>
+      <div class="flex-auto"><button class="border opacity-61 p-2 m-10 bg-slate-200 dark:bg-slate-800" value="all" phx-disable-with="⌘⌘⌘⌘⌘ ⌘⌘" phx-click="shown">catch up</button></div>
+      <div class="flex-auto"><button class="border opacity-61 p-2 m-10 bg-slate-200 dark:bg-slate-800" value="none" phx-disable-with="⎚⎚⎚⎚⎚ ⎚⎚⎚⎚⎚⎚"  phx-click="shown">start fresh</button></div>
     </div>
     """
   end
