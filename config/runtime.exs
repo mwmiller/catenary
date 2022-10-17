252631a5d807ee50b62f7a7e111b0a53459d2913
Matthew Miller <mwm@fastmail.com>
2022-10-17 12:13:34 +0200

    Update alternative clump

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,5 +4,5 @@ config :catenary,
   application_dir: "~/.catenary",
   clumps: %{
     "Quagga" => [fallback_node: [host: "quagga.nftease.online", port: 8483]],
-    "Sesigo" => [fallback_node: [host: "10.1.2.12", port: 8483]]
+    "Pitcairn" => [fallback_node: [host: "sally.nftease.online", port: 8485]]
   }
