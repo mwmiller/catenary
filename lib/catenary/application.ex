4dbe10280b9cece82e1334ae151fc43088b1163e
Matthew Miller <mwm@fastmail.com>
2022-10-24 12:20:36 +0200

    Adjust window dimensions again
    Still poking at `wxWidgets` and unable to even get the window size via
    the `elixir-desktop` items I comprehend.. much less handle a resize
    event.  I magine I'll get there, but for now it's just a bigger viewport

--- lib/catenary/application.ex
+++ lib/catenary/application.ex
@@ -48,7 +48,7 @@ defmodule Catenary.Application do
        [
          app: :catenary,
          title: "Catenary",
-         size: {1123, 701},
+         size: {1193, 761},
          id: CatenaryWindow,
          menubar: prepare_menubar("MenuBar", menu_structure()),
          url: &CatenaryWeb.Endpoint.url/0
