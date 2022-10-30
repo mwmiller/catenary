de407e06970b8601c8c0fdabef953bc1d354c748
Matthew Miller <mwm@fastmail.com>
2022-10-30 15:25:47 +0200

    Move to zebrine.net for fallback hosts

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,6 +3,6 @@ import Config
 config :catenary,
   application_dir: "~/.catenary",
   clumps: %{
-    "Quagga" => [port: 8483, fallback_node: [host: "quagga.nftease.online", port: 8483]],
-    "Pitcairn" => [port: 8485, fallback_node: [host: "sally.nftease.online", port: 8485]]
+    "Quagga" => [port: 8483, fallback_node: [host: "quagga.zebrine.net", port: 8483]],
+    "Pitcairn" => [port: 8485, fallback_node: [host: "sally.zebrine.net", port: 8485]]
   }
