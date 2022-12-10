370bc5ae1c0360cb43c5b42c6efec7cc217fe164
Matthew Miller <mwm@fastmail.com>
2022-12-10 09:31:33 +0200

    Reenable tailwind watcher
    This is mostly for others who are using in `dev` mode.  It still annoys
    the crap out of me.

--- config/dev.exs
+++ config/dev.exs
@@ -16,8 +16,8 @@ config :catenary, CatenaryWeb.Endpoint,
   secret_key_base: "5FLVVS9UwaB5UWAnrPIXBTk9eEJGr+vTvqOp742c1utBPSQxJUs6rFsmIklpCMT0",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-#    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
-#    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
+    #    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
