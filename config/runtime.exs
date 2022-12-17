81eeb8f641ef2811b1f35ee1d929706d265c052d
Matthew Miller <mwm@fastmail.com>
2022-12-17 12:31:43 +0200

    Remove Pitcairn from standard clumps

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,6 +3,5 @@ import Config
 config :catenary,
   application_dir: "~/.catenary",
   clumps: %{
-    "Quagga" => [port: 8483, fallback_node: [host: "quagga.zebrine.net", port: 8483]],
-    "Pitcairn" => [port: 8485, fallback_node: [host: "sally.zebrine.net", port: 8485]]
+    "Quagga" => [port: 8483, fallback_node: [host: "quagga.zebrine.net", port: 8483]]
   }
