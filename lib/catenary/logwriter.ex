2fadb23b0f8f4430ce194704c272c25f06cc2fe8
Matthew Miller <mwm@fastmail.com>
2023-01-13 17:08:07 +0200

    Add mention logging

--- lib/catenary/logwriter.ex
+++ lib/catenary/logwriter.ex
@@ -7,7 +7,10 @@ defmodule Catenary.LogWriter do
   """
   def new_entry(values, socket)
 
-  def new_entry(%{"body" => body, "log_id" => "360360", "title" => title} = vals, socket) do
+  def new_entry(
+        %{"body" => body, "log_id" => "360360", "title" => title} = vals,
+        socket
+      ) do
     # There will be more things to handle in short order, so this looks verbose
     # but it's probably necessary
     %Baobab.Entry{author: a, log_id: l, seqnum: e} =
@@ -16,6 +19,8 @@ defmodule Catenary.LogWriter do
       |> append_log_for_socket(360_360, socket)
 
     entry = {Baobab.Identity.as_base62(a), l, e}
+    maybe_post_mentions(body, entry, socket, Catenary.Preferences.get(:automention))
+
     Catenary.Indices.update_index(:references, [entry], socket.assigns.clump_id)
     maybe_tag(entry, vals, socket)
   end
@@ -67,6 +72,7 @@ defmodule Catenary.LogWriter do
       |> append_log_for_socket(533, socket)
 
     entry = {Baobab.Identity.as_base62(a), l, e}
+    maybe_post_mentions(body, entry, socket, Catenary.Preferences.get(:automention))
     Catenary.Indices.update_index(:references, [entry], socket.assigns.clump_id)
     maybe_tag(entry, vals, socket)
   end
@@ -221,6 +227,27 @@ defmodule Catenary.LogWriter do
     to
   end
 
+  def new_entry(%{"ref" => ref, "log_id" => "121", "mentions" => mentions}, socket) do
+    to = Catenary.string_to_index(ref)
+
+    %Baobab.Entry{author: a, log_id: l, seqnum: e} =
+      %{
+        "references" => [to],
+        "mentions" => mentions,
+        "published" => Timex.now() |> DateTime.to_string()
+      }
+      |> CBOR.encode()
+      |> append_log_for_socket(121, socket)
+
+    b62author = Baobab.Identity.as_base62(a)
+    entry = {b62author, l, e}
+    Catenary.Preferences.mark_entry(:shown, entry)
+    Catenary.Indices.update_index(:mentions, [entry], socket.assigns.clump_id)
+    Catenary.Indices.update_index(:references, [entry], socket.assigns.clump_id)
+
+    to
+  end
+
   # Punt
   def new_entry(assigns, socket) do
     # This is a debug line I keep creating, so I am
@@ -246,6 +273,52 @@ defmodule Catenary.LogWriter do
   end
 
   defp maybe_tag(entry, _, _), do: entry
+
+  defp maybe_post_mentions(text, parent, socket, true) do
+    aliases =
+      case socket.assigns.aliases do
+        {:ok, a} -> a
+        _ -> []
+      end
+
+    {:ok, re} =
+      Enum.reduce(aliases, [], fn {_k, v}, a -> ["(?:~" <> v <> ")" | a] end)
+      |> Enum.join("|")
+      |> Regex.compile()
+
+    case Regex.scan(re, text) do
+      [] ->
+        :ok
+
+      matches ->
+        found =
+          matches
+          |> List.flatten()
+          |> Enum.map(fn s -> String.replace(s, "~", "") end)
+
+        mentioned =
+          Enum.reduce(aliases, [], fn {k, v}, a ->
+            case v in found do
+              true ->
+                [k | a]
+
+              false ->
+                a
+            end
+          end)
+
+        new_entry(
+          %{
+            "log_id" => "121",
+            "ref" => Catenary.index_to_string(parent),
+            "mentions" => mentioned
+          },
+          socket
+        )
+    end
+  end
+
+  defp maybe_post_mentions(_, _, _, _), do: :ok
 
   defp append_log_for_socket(contents, log_id, socket) do
     Baobab.append_log(contents, Catenary.id_for_key(socket.assigns.identity),
