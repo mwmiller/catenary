93b464b57d8d5ce154faf78848988202eeba69bb
Matthew Miller <mwm@fastmail.com>
2023-09-14 16:23:14 +0200

    Assuage some concerns about pagniation
    This decidedly isn't the same thing, but I want to come up with a system
    which will also work for, say, tags.
    
    They aren't yet complaining about tags, so that will hold for now

--- lib/catenary_web/live/unshownexplorer.ex
+++ lib/catenary_web/live/unshownexplorer.ex
@@ -2,6 +2,7 @@ defmodule Catenary.Live.UnshownExplorer do
   require Logger
   use Phoenix.LiveComponent
 
+  @display_limit 17
   @impl true
   def update(%{which: which, clump_id: clump_id} = assigns, socket) do
     {:ok, assign(socket, Map.merge(assigns, %{card: extract(which, clump_id)}))}
@@ -17,10 +18,13 @@ defmodule Catenary.Live.UnshownExplorer do
      <div id="unshownexplore-wrap" class="col-span-2 overflow-y-auto max-h-screen m-2 p-x-2">
       <h1 class="text=center">Unshown Explorer</h1>
       <hr/>
-      <%= for {type, entries, estring} <- @card do %>
-          <h3 class="text-slate-600 dark:text-slate-300"><button phx-click="shown-set" value="<%= estring %>">∅</button>&nbsp;&nbsp;<%= type %></h3>
+      <%= for {type, entries, estring, size} <- @card do %>
+        <h3 class="text-slate-600 dark:text-slate-300"><button phx-click="shown-set" value="<%= estring %>">∅</button>&nbsp;&nbsp;<%= type %></h3>
         <div class="grid grid-cols-5 my-2">
         <%= entries %>
+        <%= if size == :more do %>
+        <p class="text-xs">(◎+)</p>
+        <% end %>
         </div>
       <% end %>
      </div>
@@ -49,13 +53,18 @@ defmodule Catenary.Live.UnshownExplorer do
 
   defp prettify([], acc), do: acc
 
-  defp prettify([{k, v} | rest], acc),
-    do:
-      prettify(rest, [
-        {Catenary.pretty_log_name(k), icon_entries(Enum.take(v, 17)),
-         Catenary.index_list_to_string(v)}
-        | acc
-      ])
+  defp prettify([{k, v} | rest], acc) do
+    size =
+      case length(v) > @display_limit do
+        true -> :more
+        false -> :all
+      end
+
+    display_list = v |> Enum.sort_by(&elem(&1, 2)) |> Enum.take(@display_limit) |> icon_entries()
+    group = {Catenary.pretty_log_name(k), display_list, Catenary.index_list_to_string(v), size}
+
+    prettify(rest, [group | acc])
+  end
 
   defp icon_entries(entries) do
     entries
