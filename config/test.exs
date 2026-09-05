e695ad036c0e134c69046803035d25e23f74b4a9
Matthew Miller <mwm@fastmail.com>
2026-09-05 17:17:09 -0400

    Update test hygiene

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,10 @@
 import Config
 
+# Tests must be hermetic: never read or write the user's real ~/.catenary.
+# Persisted view/entry preferences would otherwise select which render/1
+# clause mounts "/" with, breaking tests that assume the default view.
+config :catenary, application_dir: Path.expand("~/.catenary-test")
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :catenary, CatenaryWeb.Endpoint,
