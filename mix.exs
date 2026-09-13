a4065e4b8066bcc7c08be5aeeb1b6f3f2720176f
Matthew Miller <mwm@fastmail.com>
2026-09-13 16:15:48 -0400

    Bump version to 0.107.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Catenary.MixProject do
   def project do
     [
       app: :catenary,
-      version: "0.103.0",
+      version: "0.107.0",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
